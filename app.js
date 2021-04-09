72243c496455e2e6d4f3535d470ec96283c00d23
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-04-09 19:26:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/',(req, res)=> {
+    res.render('index')
+})
+
+
+const PORT =  process.env.PORT || 3000
+app.listen(PORT, ()=>{
+    console.log("Server Started.")
+});
